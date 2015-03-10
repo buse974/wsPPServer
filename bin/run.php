42a0fe4d98062c34f761a83a68f956e06788dfa5
Christophe Robert <crobert@thestudnet.com>
2015-03-10 20:54:07 +0100

    update config

--- bin/run.php
+++ bin/run.php
@@ -6,13 +6,29 @@ use Ratchet\WebSocket\WsServer;
 
     require dirname(__DIR__) . '/vendor/autoload.php';
 
+    $options = array('h' => '0.0.0.0', 'p' => '8080');
+    $f_option = dirname(__DIR__) . '/config/local.php';
+    if(file_exists($f_option)) {
+    	$config =  include $f_option;
+    	if(!empty($config['address'])) {
+    		$options['h'] = $config['address'];
+    	}
+    	if(!empty($config['port'])) {
+    		$options['p'] = $config['port'];
+    	}
+    } else {
+    	$options = array_merge($options, getopt("h:p:"));
+    }
+    
     $server = IoServer::factory(
         new HttpServer(
             new WsServer(
                 new PushPull()
             )
         ),
-        8080
+        $options['p'],
+    	$options['h']
     );
 
+    echo "Listen >>> " . $options['h'] . ":" . $options['p'] . "\n";
     $server->run();
--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-php bin/run.php
+HOST=""
+PORT=""
+
+while getopts "h:p:" option
+do
+    case $option in
+        h)
+            HOST="-h "$OPTARG
+        ;;
+        p)
+            PORT="-p "$OPTARG
+        ;;	
+    esac
+done
+
+DIR=`dirname "$(realpath $0)"`
+php $DIR/run.php $HOST $PORT
